7846a184403770b00a60366f402b8cc54453bdc5
TheVipek <thevipek0552@gmail.com>
2024-05-15 23:36:31 +0200

    Text
    *changed rope<char> to std::vector<rope<char>>, it would probably result in overhead with modifying those ropes later, but its much easier to maintain it in this form
    *reimplemented handling events based on it
    *text is rendered line by line, instead all at once (it might allow for cool optimizing later? for example rendering only those that are visible to user?)
    
    its time to introduce tests for TextEdit (maybe other components also?), due to large amount of cases + overall higher complexity

--- include/Renderer/TextEdit.h
+++ include/Renderer/TextEdit.h
@@ -16,38 +16,34 @@ using namespace __gnu_cxx;
 struct Cursor {
     int             Column = 0;
     int             Line = 0;
-    int             Index = 0;
 };
 
 class TextEdit : public SigmaRenderableObject, public IText, public IBackground
 {
 public:
     TextEdit(SDL_Rect& rect, std::shared_ptr<IWindowRenderingManager> targetWindow, rope<char> text)
-        : SigmaRenderableObject(rect, targetWindow),text(text){
+        : SigmaRenderableObject(rect, targetWindow){
         initFont(DEFAULT_FONTP, DEFAULT_FONTS);
-
         TTF_SizeUTF8(font->get(), " ",&letterWidth, &letterHeight);
+        setText(text.c_str());
     }
     void setText(const std::string& text) override {
-        //this->text.push_back(rope<char>(text.c_str()));
-        this->text = rope<char>(text.c_str());
+        this->text = { rope<char>(text.c_str()) };
     }
     std::string getText() override {
-        // std::string t = "";
-        // for (auto _text: text) {
-        //     t += _text.c_str();
-        // }
-        // return t;
-        return text.c_str();
+        std::string t = "";
+        for (auto _text: text) {
+            t += _text.c_str();
+        }
+        return t;
     }
 
     void handleEvent(const SDL_Event &e) override;
     void render(SDL_Renderer* renderer) override;
 protected:
     int getLengthOfColumn(const int& column);
-    rope<char>                 text; // temporary, need to write custom data structure
+    std::vector<rope<char>>    text = {}; // temporary, need to write custom data structure
     bool                       isActive = false;
-    std::vector<int>           lines = { 0 };
     Cursor                     cursor = {};
     int                        letterWidth;
     int                        letterHeight;
--- src/Renderer/TextEdit.cpp
+++ src/Renderer/TextEdit.cpp
@@ -34,128 +34,155 @@ void TextEdit::handleEvent(const SDL_Event &e) {
     //Writing
     if(isActive) {
         switch (e.type) {
-            case SDL_KEYDOWN: {
-                int keycode = e.key.keysym.sym;
-                if (keycode == SDLK_BACKSPACE && !text.empty()) {
-                    if(text[text.length() - 1] == '\n') {
-                        cursor.Column--;
-                        cursor.Line--;
-                    }
-                    else if(lines[cursor.Column] > 0) {
-                        lines[cursor.Column]--;
-                        cursor.Line--;
-                    }
-                    text.erase(cursor.Line, 1);
-                } else if (keycode == SDLK_TAB) {
-                    text.insert(cursor.Line,"   ");
-                    lines[cursor.Column]+=3;
-                    cursor.Line+=3;
-                } else if (keycode == SDLK_RETURN) {
-
-                    text.insert(cursor.Line,"\n");
-                    if(lines.size() <= cursor.Column + 1) {
-                        lines.push_back(0);
-                    }
-                    cursor.Column++;
-                    cursor.Line++;
-                }
-                else if (keycode == SDLK_LEFT) {
-                    if(cursor.Line + cursor.Index <= 0) {
-                        printf("breaking... \n");
-                        break;
-                    }
-                    cursor.Index--;
-
-                    if(text[cursor.Line + cursor.Index] == '\n') {
-                        printf("breakline appeared; \n");
-                        cursor.Column--;
-                        cursor.Index = 0;
-                        cursor.Line = lines[cursor.Column];
-                    }
-                }
-                // else if(keycode == SDLK_RIGHT) {
-                //     printf("RIGHT \n");
-                //     if(cursor.Line + cursor.Index >= text.length()) {
-                //         printf("breaking... \n");
-                //         break;
-                //     }
-                //     cursor.Index++;
-                //
-                //     // if(text[cursor.Line + cursor.Index] == '\n') {
-                //     //     printf("breakline appeared; \n");
-                //     //     cursor.Column++;
-                //     //     cursor.Index = -lines[cursor.Column];
-                //     //     cursor.Line = lines[cursor.Column];
-                //     // }
-                //
-                //     if (text[cursor.Line + cursor.Index] == '\n') {
-                //         printf("breakline appeared; \n");
-                //         cursor.Column++;
-                //         cursor.Index = 0;
-                //         cursor.Line++;
-                //     } else if (cursor.Index > lines[cursor.Column]) {
-                //         cursor.Index = 0;
-                //         cursor.Column++;
-                //         cursor.Line++;
-                //     }
-                //
-                //     printf("cursor.Column %d \n", cursor.Column);
-                //     printf("cursor.Index %d \n", cursor.Index);
-                //     printf("cursor.Line %d \n", cursor.Line);
-                // }
-                break;
-            }
             case SDL_TEXTINPUT: {
-                text.insert(cursor.Line + cursor.Index,e.text.text);
-                lines[cursor.Column]++;
+                text[cursor.Column].insert(cursor.Line,e.text.text);
                 cursor.Line++;
                 break;
             }
+            case SDL_KEYDOWN: {
+                int keycode = e.key.keysym.sym;
+
+                auto currentTextLine = text[cursor.Column];
+                if (keycode == SDLK_BACKSPACE) {
+
+                    if(currentTextLine.empty()) {
+                        if(cursor.Column == 0) {
+                            break;
+                        }
+
+                        cursor.Column--;
+                        cursor.Line = text[cursor.Column].size();
+                    }
+                    else {
+                        cursor.Line--;
+                        currentTextLine.erase(cursor.Line, 1);
+                    }
+                }
+                else if (keycode == SDLK_TAB) {
+                    currentTextLine.insert(cursor.Line,"   ");
+                    cursor.Line+=3;
+                }
+                else if (keycode == SDLK_RETURN) {
+                    //currentTextLine.insert(cursor.Line,"\n");  // no need for it?
+
+                    if(text.size() - 1 == cursor.Column) { // last index
+                        //printf("LAST textSize %d;cursor.Column %d  \n", text.size(), cursor.Column);
+                        if(cursor.Line < currentTextLine.size()) {
+                            //printf("smaller \n");
+                            rope<char> newLineText = currentTextLine.substr(cursor.Line, currentTextLine.size() - cursor.Line);
+                            text.push_back(newLineText);
+
+                            rope<char> modifiedCurrentLine = currentTextLine.substr(0, cursor.Line);
+                            text[cursor.Column] = modifiedCurrentLine;
+                        }
+                        else {
+                            text.emplace_back();
+                        }
+                    }
+                    else if(text.size() - 1 > cursor.Column) { // between index
+                        //printf("BETWEEN textSize %d;cursor.Column %d  \n", text.size(), cursor.Column);
+                        if(cursor.Line < currentTextLine.size()) {
+                            //printf("smaller \n");
+                            rope<char> newLineText = currentTextLine.substr(cursor.Line, currentTextLine.size() - cursor.Line);
+                            text.insert(text.begin() + cursor.Column + 1,newLineText);
+
+                            rope<char> modifiedCurrentLine = currentTextLine.substr(0, cursor.Line);
+                            text[cursor.Column] = modifiedCurrentLine;
+                        }
+                        else {
+                            //printf("insert between \n");
+                            text.insert(text.begin() + cursor.Column + 1, rope<char>(""));
+                        }
+                    }
+                    cursor.Column++;
+                    cursor.Line = text[cursor.Column].size();
+                }
+                else if (keycode == SDLK_LEFT) {
+                    if(cursor.Column == 0 && cursor.Line == 0) {
+                        //printf("Beginning of document \n");
+                        break;
+                    }
+
+                    if(cursor.Line == 0) {
+                        //printf("Detected newline character");
+                        cursor.Column--;
+                        cursor.Line = text[cursor.Column].size();
+                    }
+                    else {
+                        cursor.Line--;
+                    }
+                }
+                else if(keycode == SDLK_RIGHT) {
+                    if(cursor.Column == text.size() && cursor.Line == currentTextLine.size()) {
+                        printf("End of document \n");
+                        break;
+                    }
+
+                    if(cursor.Line == currentTextLine.size()) {
+                        cursor.Column++;
+                        cursor.Line = 0;
+                    }
+                    else {
+                        cursor.Line++;
+                    }
+                }
+            }
         }
+        printf("Line; %d \n", cursor.Line);
     }
 }
 
 void TextEdit::render(SDL_Renderer *renderer) {
-    if(!visible)
+    if (!visible)
         return;
 
     SigmaRenderableObject::render(renderer);
 
-    //BG
+    // Background rendering
     SDL_SetRenderDrawColor(renderer, backgroundColor.r, backgroundColor.g, backgroundColor.b, backgroundColor.a);
     SDL_RenderFillRect(renderer, &currentRect);
 
-
-    //FONT RENDER
-    if(font->get() == nullptr || strlen(text.c_str()) == 0)
+    // Font rendering
+    if (font->get() == nullptr || text.empty())
         return;
 
-    SDL_Surface* surface = TTF_RenderUTF8_Blended_Wrapped(font->get(), text.c_str(), textColor, 0);
-    if (surface == nullptr) {
-        SDL_Log("Unable to create text surface: %s", SDL_GetError());
-        return;
-    }
-
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-    if (texture == nullptr) {
-        SDL_Log("Unable to create texture: %s", SDL_GetError());
-        SDL_FreeSurface(surface);
-        return;
-    }
-
-    SDL_Rect textRect = {currentRect.x, currentRect.y,  surface->w , surface->h};
-
-
+    // Get font line height and calculate space between lines
     int spaceBetweenLine = TTF_FontLineSkip(font->get());
-    SDL_RenderCopy(renderer, texture, NULL, &textRect);
+    int yOffset = 0;
 
+    // Render each line of text
+    for (size_t i = 0; i < text.size(); ++i) {
+        const auto& lineRope = text[i];
+        std::string line(lineRope.c_str(), lineRope.size());
+        if (line.empty())
+            line = " "; // to render even empty lines correctly
+
+        SDL_Surface* surface = TTF_RenderUTF8_Blended_Wrapped(font->get(), line.c_str(), textColor, 0);
+        if (surface == nullptr) {
+            SDL_Log("Unable to create text surface: %s", SDL_GetError());
+            continue;
+        }
+
+        SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+        if (texture == nullptr) {
+            SDL_Log("Unable to create texture: %s", SDL_GetError());
+            SDL_FreeSurface(surface);
+            continue;
+        }
+        SDL_Rect textRect = {currentRect.x, currentRect.y + yOffset, surface->w, surface->h};
+        SDL_RenderCopy(renderer, texture, nullptr, &textRect);
+
+        SDL_DestroyTexture(texture);
+        SDL_FreeSurface(surface);
+
+        yOffset += spaceBetweenLine;
+    }
+
+    // Render cursor
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    int cursorPosX = (currentRect.x + offset.Left) + (letterWidth * (lines[cursor.Column] + cursor.Index));
-    int cursorPosY = (currentRect.x + offset.Top) + (cursor.Column * spaceBetweenLine);
+
+    int cursorPosX = currentRect.x + (letterWidth * cursor.Line);
+    int cursorPosY = currentRect.y + (cursor.Column * spaceBetweenLine);
 
     SDL_RenderDrawLine(renderer, cursorPosX, cursorPosY, cursorPosX, cursorPosY + spaceBetweenLine);
-
-    SDL_DestroyTexture(texture);
-    SDL_FreeSurface(surface);
-    //SDL_Log("Text rendered at: %d, %d, %d, %d", textRect.x, textRect.y, textRect.w, textRect.h);
 }
